1baa48e4f37009dc08410d6a6ca25e7af7a6947f
najdek <4890607+najdek@users.noreply.github.com>
2026-05-02 13:56:19 +0200

    release: v0.1.1 - stability and ui refinements

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## v0.1.1
+
+- Resolved legacy WebKit (PS5 4.03 / Safari 12) compatibility bugs.
+- Improved autoload countdown synchronization.
+
+
 ## v0.1.0
 
 - Initial release
--- include/pldmgr.h
+++ include/pldmgr.h
@@ -33,7 +33,7 @@
 
 
 
-#define MENU_VERSION "0.1.0"
+#define MENU_VERSION "0.1.1"
 #define AUTOLOAD_CONFIG_PATH "/data/pldmgr/autoload.txt"
 #define PLDMGR_CONFIG_PATH "/data/pldmgr/pldmgr_config.txt"
 #define REPOSITORY_CACHE_PATH "/data/pldmgr/repository_cache.json"
